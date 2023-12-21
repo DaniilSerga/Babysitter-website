c0b79accdbfe50209b20165c23b2234b9605398b
daniilsergx <87036978+DaniilSerga@users.noreply.github.com>
2023-12-21 21:37:58 +0300

    init: first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Asd</p>
+    </div>
+  );
+}
+
+export default App;
