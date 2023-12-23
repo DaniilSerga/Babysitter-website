96b56a13984fe0e2f032b149d66b3e6b3c0e155f
daniilsergx <87036978+DaniilSerga@users.noreply.github.com>
2023-12-24 00:15:48 +0300

    feat: add achievments

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,9 @@
 import logo from './logo.svg';
+import professionalismIcon from './professionalismIcon.svg'
+import childEducationIcon from './childEducationIcon.svg';
+import flexIcon from './flexIcon.svg';
+import careIcon from './careIcon.svg';
+import privacyIcon from './privacyIcon.svg';
+import experienceIcon from './experienceIcon.svg';
 
-export {logo};
+export {logo, professionalismIcon, childEducationIcon, flexIcon, careIcon, privacyIcon, experienceIcon};
---
+++ src/pages/AchievementsSection/AchievementsSection.tsx
@@ -0,0 +1,45 @@
+import React, {FC} from 'react';
+
+import styles from './AchievementsSection.module.scss';
+import { careIcon, childEducationIcon, experienceIcon, flexIcon, privacyIcon, professionalismIcon } from 'assets';
+
+const AchievementsSection: FC = () => {
+    return (
+        <section className={styles.sectionContainer}>
+            <ul className={styles.contentList}>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={professionalismIcon} alt="" /></div>
+                    <h4>Профессионализм</h4>
+                    <p>Наши няни прошли строгий отбор и обладают опытом работы с детьми</p>
+                </li>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={childEducationIcon} alt="" /></div>
+                    <h4>Развитие ребёнка</h4>
+                    <p>Няня не только присмотрит за ребенком, но и поможет ему развиваться</p>
+                </li>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={flexIcon} alt="" /></div>
+                    <h4>Гибкость и удобство</h4>
+                    <p>Мы предлагаем гибкие графики работы нянь, чтобы вы могли заниматься своими делами</p>
+                </li>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={careIcon} alt="" /></div>
+                    <h4>Любовь и забота</h4>
+                    <p>Наши няни проявляют любовь и заботу к детям</p>
+                </li>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={privacyIcon} alt="" /></div>
+                    <h4>Конфиденциальность</h4>
+                    <p>Гарантия конфиденциальности и безопасности данных</p>
+                </li>
+                <li className={styles.listItem}>
+                    <div className={styles.iconContainer}><img src={experienceIcon} alt="" /></div>
+                    <h4>Опыт и репутация</h4>
+                    <p>Агенство имеет опыт и хорошую репутацию в своей сфере</p>
+                </li>
+            </ul>
+        </section>
+    );
+};
+
+export default AchievementsSection;
