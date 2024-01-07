597a91115c0e7c93c47dd3badb0a84704e6504a6
daniilsergx <87036978+DaniilSerga@users.noreply.github.com>
2024-01-07 22:04:40 +0300

    Feat/home section (#7)
    * init: add mixins
    
    * feat: make adaptive for home section

--- src/components/FeedbackDivider/FeedbackDivider.tsx
+++ src/components/FeedbackDivider/FeedbackDivider.tsx
@@ -1,13 +1,23 @@
 import React, {FC} from 'react';
 
 import styles from './FeedbackDivider.module.scss';
+import { checkMarkIcon, personIcon, starIcon } from 'assets';
 
 const FeedbackDivider: FC = () => {
     return (
         <section className={styles.dividerContainer}>
-            <p>Няни с опытом работы</p>
-            <p>Довольные родители</p>
-            <p>Качественный подбор</p>
+            <div>
+                <img src={starIcon} alt="star icon" />
+                <p>Няни с опытом работы</p>
+            </div>
+            <div>
+                <img src={checkMarkIcon} alt="check mark icon" />
+                <p>Довольные родители</p>
+            </div>
+            <div>
+                <img src={personIcon} alt="person icon" />
+                <p>Качественный подбор</p>
+            </div>
         </section>
     );
 };
